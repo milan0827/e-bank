5f2ce729775f982aa9f7424d9a288970564caf41
milan0827 <milanrokaya2014@gmail.com>
2024-11-28 02:34:53 +0545

    create account unit test

--- controller/account.controller.test.ts
+++ controller/account.controller.test.ts
@@ -1,50 +1,58 @@
 import request from 'supertest';
 import app from '../app';
+import accountsController from './accounts.controller';
+import { NextFunction, Request, Response } from 'express';
+import { db } from '../db/drizzle';
+jest.mock('../db/drizzle.ts');
 
-test('Returns the list of account', async () => {
-  const res = await request(app).get('/api/v1/accounts');
-  console.log('res', res.body);
-  expect(res.status).toBe(200);
-  expect(res.body).not.toBeNull();
-  expect(res.body.data.length).toBeGreaterThan(0);
-  expect(res.body.status).toEqual('success');
-});
-
-// describe('Creating account', () => {
-//   const mockAccount = {
-//     owner: 'Test Account',
-//     balance: 10000,
-//     currency: 'NRS',
-//   };
-//   const req = {
-//     body: { owner: 'Test Account', currency: 'NRS', balance: 10000 },
-//   } as unknown as Request;
-
-//   const res = {
-//     status: jest.fn().mockReturnThis(),
-//     json: jest.fn(),
-//   } as unknown as Response;
-
-//   const next = jest.fn();
-
-//   it('Should create an account and send back response', async () => {
-//     const mockSaveAccount = jest.fn().mockResolvedValue(mockAccount);
-
-//     await accountsController.createAccount(req, res, next);
-//     expect(mockSaveAccount).toHaveBeenCalledWith(expect.objectContaining({ balance: 10000, currency: 'NRS', owner: 'Test Account' }));
-//     expect(res.status).toHaveBeenCalled();
-//     expect(res.json).toHaveBeenCalledWith(mockAccount);
-//     expect(next).toHaveBeenCalled();
-//   });
-
-//   it('Should call next with an error if account creation fails', async () => {
-//     const error = new Error('Database error');
-//     const mockSaveAccount = jest.fn().mockRejectedValue(error);
-//     await accountsController.createAccount(req, res, next);
-
-//     expect(mockSaveAccount).toHaveBeenCalledWith(expect.objectContaining({ balance: 10000, currency: 'NRS', owner: 'Test Account' }));
-//     expect(res.status).not.toHaveBeenCalled();
-//     expect(res.json).not.toHaveBeenCalled();
-//     expect(next).toHaveBeenCalledWith(error);
-//   });
+// test('Returns the list of account', async () => {
+//   const res = await request(app).get('/api/v1/accounts');
+//   console.log('res', res.body);
+//   expect(res.status).toBe(200);
+// expect(res.body).not.toBeNull();
+// expect(res.body.data.length).toBeGreaterThan(0);
+// expect(res.body.status).toEqual('success');
 // });
+
+describe('POST /api/v1/accounts ', () => {
+  it('Shoud return the list of account', async () => {
+    const res = await request(app).get('/api/v1/accounts');
+    console.log(res.body, 'Res');
+
+    expect(res.statusCode).toBe(200);
+    expect(res.body).not.toBeNull();
+    expect(res.body.data.length).toBeGreaterThan(0);
+    expect(res.body.status).toEqual('success');
+  }),
+    it('Should create account successfully', async () => {
+      const mockResult = { currency: 'NRS', balance: 100, fullName: 'Test user' };
+      // Mocking db insert behavior
+      db.insert = jest.fn().mockImplementation(() => ({
+        values: () => ({
+          returning: async () => mockResult,
+        }),
+      }));
+
+      // Mock req and res
+      const req = {
+        body: {
+          fullName: 'Test user',
+          balance: 100,
+          currency: 'NRS',
+        },
+      } as Request;
+
+      const status = jest.fn().mockReturnThis();
+      const json = jest.fn();
+      const res = {
+        status,
+        json,
+      } as unknown as Response;
+
+      const nextFn = jest.fn() as NextFunction;
+
+      await accountsController.createAccount(req, res, nextFn);
+
+      expect(status).toHaveBeenCalledWith(200);
+    });
+});
