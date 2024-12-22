f3ffad89f0914f5d5be25be6912fcec7463d3588
milan0827 <milanrokaya2014@gmail.com>
2024-12-23 00:40:37 +0545

    token creation and verifcation function

---
+++ token/jwt_token.ts
@@ -0,0 +1,29 @@
+import { NextFunction, Request, Response } from 'express';
+import jwt from 'jsonwebtoken';
+import CustomError from '../helpers/customError';
+
+const JWT_SECRET_KEY_LENGTH = 32;
+
+export function createToken(username: string) {
+  if (process.env.JWT_SECRET_KEY!.length < JWT_SECRET_KEY_LENGTH) {
+    throw new Error('secret key length must be of 32 characters');
+  }
+
+  jwt.sign({ username }, process.env.JWT_SECRET_KEY!, {
+    expiresIn: '1h',
+  });
+}
+
+export function verifyToken(req: Request, res: Response, next: NextFunction) {
+  const bearerToken = req.header('Authorization');
+  if (!bearerToken) return next(new CustomError('token verification failed', 401));
+
+  try {
+    const token = bearerToken?.split(' ')[1];
+    const decoded = jwt.verify(token as string, process.env.JWT_SECRET_KEY!) as { username: string };
+    req.body.username = decoded.username;
+    next();
+  } catch (error) {
+    return next(new CustomError('invalid token', 401));
+  }
+}
