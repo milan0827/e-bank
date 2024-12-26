358fb22f301f944457fcbca89698a53d35efb8b0
milan0827 <milanrokaya2014@gmail.com>
2024-12-27 03:40:33 +0545

    account rooutes protected

--- controller/accounts/accounts.controller.ts
+++ controller/accounts/accounts.controller.ts
@@ -1,9 +1,11 @@
 import { NextFunction, Request, Response } from 'express';
 import { db } from '../../db/drizzle';
-import { accounts, AccountType } from '../../db/schema';
+import { accounts, AccountType, users } from '../../db/schema';
 import { eq } from 'drizzle-orm';
 import { catchAsynncFunc } from '../../helpers/catchAysynFunc';
 import CustomError from '../../helpers/customError';
+
+const ACCOUNT_NOT_FOUND_ERROR = 'account not found';
 
 enum currencyEnum {
   NRS = 'NRS',
@@ -16,8 +18,12 @@ const getAccount = catchAsynncFunc(async (req: Request, res: Response, next: Nex
 
   const accountExists = await db.select().from(accounts).where(eq(accounts.id, +id)).limit(1);
 
+  if (accountExists[0].owner !== req.body.username) {
+    return next(new CustomError('account not found', 401));
+  }
+
   if (!accountExists || accountExists.length === 0) {
-    return next(new CustomError('Account with that id does not exists', 400));
+    return next(new CustomError('account not found', 400));
   }
 
   res.status(200).json({
@@ -28,10 +34,10 @@ const getAccount = catchAsynncFunc(async (req: Request, res: Response, next: Nex
 });
 
 const getAllAccounts = catchAsynncFunc(async (req: Request, res: Response, next: NextFunction) => {
-  const allAccounts = await db.select().from(accounts).limit(5).orderBy(accounts.id);
+  const allAccounts = await db.select().from(accounts).where(eq(accounts.owner, req.body.username)).limit(5).orderBy(accounts.id);
 
   if (!allAccounts) {
-    return next(new CustomError('No account found', 200));
+    return next(new CustomError('accounts not found', 200));
   }
 
   res.status(200).json({
@@ -41,21 +47,25 @@ const getAllAccounts = catchAsynncFunc(async (req: Request, res: Response, next:
   });
 });
 
-const createAccount = catchAsynncFunc(async (req: Request, res: Response) => {
+const createAccount = catchAsynncFunc(async (req: Request, res: Response, next: NextFunction) => {
   const { owner, balance, currency }: AccountType = req.body;
 
   if (!owner || typeof owner !== 'string' || owner.trim().length === 0) {
     return res.status(400).json({
       status: 'fail',
-      message: 'Full name is required',
+      message: 'full name is required',
     });
   }
 
   if (!Object.values(currencyEnum).includes(currency as currencyEnum)) {
     return res.status(400).json({
       status: 'fail',
-      message: 'Currency',
+      message: 'not allowed, accounts with two currency',
     });
+  }
+
+  if (owner !== req.body.username) {
+    return next(new CustomError('not authorized', 401));
   }
 
   const account = await db
@@ -79,12 +89,16 @@ const updateBalance = catchAsynncFunc(async (req: Request, res: Response, next: 
   const { id } = req.params;
   const accountExists = await db.select().from(accounts).where(eq(accounts.id, +id)).limit(1);
 
+  if (accountExists[0].owner !== req.body.username) {
+    return next(new CustomError('not authorized', 401));
+  }
+
   if (accountExists.length === 0) {
-    return next(new CustomError('Account with that id does not exists', 400));
+    return next(new CustomError('account with that id does not exists', 400));
   }
 
   if (!req.body.balance) {
-    return next(new CustomError('The balance field can not be field', 400));
+    return next(new CustomError('the balance field can not be field', 400));
   }
 
   const updatedBalanceInfo = await db.update(accounts).set({ balance: req.body.balance }).where(eq(accounts.id, +id)).returning({
@@ -101,11 +115,13 @@ const updateBalance = catchAsynncFunc(async (req: Request, res: Response, next: 
 });
 
 const deleteAccount = catchAsynncFunc(async (req: Request, res: Response, next: NextFunction) => {
+  // TODO: Create admin, Only the admin can delete the account after requested by user
+
   const { id } = req.params;
   const accountExists = await db.select().from(accounts).where(eq(accounts.id, +id)).limit(1);
 
   if (accountExists.length === 0) {
-    return next(new CustomError('Account with that id does not exists', 400));
+    return next(new CustomError('account with that id does not exists', 400));
   }
 
   await db.delete(accounts).where(eq(accounts.id, +id)).returning();
