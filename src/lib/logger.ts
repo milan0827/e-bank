efc16ec737a089c6453812a5b8459c11864df66c
milan0827 <milanrokaya2014@gmail.com>
2024-12-28 03:10:38 +0545

    testing

---
+++ src/lib/logger.ts
@@ -0,0 +1,3 @@
+import winston from 'winston';
+
+// const levels =
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
 import app from './app';
 
-app.listen(3000, () => {
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
   console.log('listening on port 3000...');
 });
