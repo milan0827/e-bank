e635ea7059733a53754f81712c5a07468c557bd8
milan0827 <milanrokaya2014@gmail.com>
2024-12-27 22:30:58 +0545

    validation using zod

--- routes/transfers.routes.ts
+++ routes/transfers.routes.ts
@@ -1,12 +1,15 @@
 import express from 'express';
 import transferController from '../controller/transfers/transfers.controller';
 import authMiddleware from '../middleware/auth';
+import { validateData } from '../middleware/validateData';
+import { CreateTransferSchema } from '../controller/transfers/schema/transfers.schema';
 
 const router = express.Router();
 
 router.use(authMiddleware);
-router.route('/').post(transferController.createTransfer).get(transferController.getAllTransfer);
+router.route('/').post(validateData(CreateTransferSchema), transferController.createTransfer).get(transferController.getAllTransfer);
 
+router.route('/:accountId').get(transferController.getOneAccountTransfers);
 router.route('/:id').get(transferController.getTransfer);
 
 export default router;
